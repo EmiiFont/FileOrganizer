cfd2313f5737a0ef6dad8a31f894748bd0857795
EmiiFont <emifont01@gmail.com>
2013-10-18 15:38:46 -0400

    Updated Readme

---
+++ ChainOfResponsability/ChainOfResponsability/FileProcessor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsability.ChainOfResponsability
+{
+   public class FileProcessor
+   {
+       private IFileHandler _firstHandler;
+       private IFileHandler _succesorHandler;
+       /// <summary>
+       /// Mantein the reference to the previous handler so we can add a new one.
+       /// </summary>
+       /// <param name="handler"></param>
+       public void AddHandler(IFileHandler handler)
+       {
+           if (_firstHandler == null)
+           {
+               _firstHandler = handler;
+           }
+           else
+           {
+               _succesorHandler.NextHandler(handler);
+           }
+           _succesorHandler = handler;
+
+       }
+
+       public void HandleFile(AddedFile file)
+       {
+           _firstHandler.HandleRequest(file);
+       }
+
+   }
+}
---
+++ ChainOfResponsability/FileHandlers/TextFileHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChainOfResponsability.Attribute;
+using ChainOfResponsability.ChainOfResponsability;
+using ChainOfResponsability.Helpers;
+
+namespace ChainOfResponsability.FileHandlers
+{
+    public class TextFileHandler : IFileHandler
+    {
+        private IFileHandler _nextHandler;
+
+        public void NextHandler(IFileHandler handler)
+        {
+            _nextHandler = handler;
+        }
+
+        public void HandleRequest(AddedFile file)
+        {
+            if (!file.Extension.Equals(StringEnum.GetStringValue(FileExtension.Txt), 
+                StringComparison.CurrentCultureIgnoreCase))
+            {
+                if (_nextHandler == null)
+                {
+                    return;
+                }
+                _nextHandler.HandleRequest(file);
+            }
+            else
+            {
+               MoveFileHelper.ProcessFile(file);
+            }
+        }
+    }
+}
